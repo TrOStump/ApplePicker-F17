de1f85bac87cd67a0d15106ae7d85f4ec65db300
TrOStump <TrOates97@gmail.com>
2017-09-22 11:47:29 -0400

    got to a point where code would not work, ask Crosby for assistance

---
+++ Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePicker : MonoBehaviour {
+
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+	// Update is called once per frame
+	void Start() {
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGo = Instantiate(basketPrefab) as GameObject;
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGo.transform.position = pos;
+        }
+	}
+    void Update()
+    {
+        //get the current screen position of the mouse from input
+        Vector3 mousePos2D = Input.mousePosition;
+        // the Camera's z position sets how far to push the mouse into 3d
+        mousePos2D.z = -Camera.main.transform.position.z;
+        // Convert the point from 2D screen space into 3d game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        //move the x position of the basket to the x position of the mouse
+        Vector3 posi = this.transform.position;
+        posi.x = mousePos3D.x;
+        this.transform.position = posi;
+    }
+    
+}
---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+
+    // prefabs for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed at which the apple tree moves
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float LeftAndRightEdge = 10f;
+
+    //Chance that the AppleTree will change directions
+    public float changeToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instantiated
+    public float secondBetweenAppleDrops = 1f;
+    void Start()
+    {
+        InvokeRepeating("DropApple", 2f, secondBetweenAppleDrops);
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+    }
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if (pos.x < -LeftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //move right
+        }
+        else if (pos.x > LeftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //move left
+
+        }
+
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < changeToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
+    }
+}
