32113d2f84ad6421198208422344cfe47b533824
TrOStump <TrOates97@gmail.com>
2017-09-22 11:52:58 -0400

    fixed it, ask crosby about collisions tho

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -18,18 +18,6 @@ public class ApplePicker : MonoBehaviour {
             tBasketGo.transform.position = pos;
         }
 	}
-    void Update()
-    {
-        //get the current screen position of the mouse from input
-        Vector3 mousePos2D = Input.mousePosition;
-        // the Camera's z position sets how far to push the mouse into 3d
-        mousePos2D.z = -Camera.main.transform.position.z;
-        // Convert the point from 2D screen space into 3d game world space
-        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
-        //move the x position of the basket to the x position of the mouse
-        Vector3 posi = this.transform.position;
-        posi.x = mousePos3D.x;
-        this.transform.position = posi;
-    }
+    
     
 }
